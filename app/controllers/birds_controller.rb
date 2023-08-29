32f28478544856cd6934b01662aa0b4eacdd6e06
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-08-29 14:52:10 -0400

    commit tree 565563d2fffde122c042b9ba7718de68e7e7977e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   # GET /birds
   def index
     birds = Bird.all
@@ -12,25 +14,19 @@ class BirdsController < ApplicationController
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    render json: bird
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
   end
 
-  # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
+  rescue ActiveRecord::RecordNotFound
+      render_not_found_response
   end
 
   # PATCH /birds/:id/like
@@ -59,6 +55,14 @@ class BirdsController < ApplicationController
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def find_bird
+    Bird.find(params[:id])
   end
 
 end
